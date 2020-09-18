f137e84ad510935edc31aa1d69a59386c020dc4c
WebCrew <palareas@gmail.com>
2020-09-18 05:50:46 +0200

    Initial commit

---
+++ info.php
@@ -0,0 +1,10 @@
+<?php
+
+$info['name'] = 'contrast';
+$info['description'] = 'A black & white theme for Codoforum';
+$info['version'] = '1.x';
+$info['author'] = "WebCrew";
+$info['author_url'] = 'https://github.com/WebCrew';
+$info['license'] = 'GNU Affero General Public License v3.0';
+$info['core'] = '1.x';
+$info['parent_theme'] = 'default';
